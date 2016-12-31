eea4c18c23265011f2b3271bccb205d02cdef5de
Mark F Guerra <markfguerra@gmail.com>
2016-12-30 23:23:49 -0500

    Create class WikipediaService

---
+++ spring-app-demo/src/main/java/net/markguerra/springappdemo/services/WikipediaService.java
@@ -0,0 +1,13 @@
+package net.markguerra.springappdemo.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Gets information from Wikipedia through the MediaWiki API
+ */
+@Service
+public class WikipediaService {
+	public String getArticle(String title) {
+		return null;
+	}
+}
